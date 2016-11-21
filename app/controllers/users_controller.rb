99caa9c8353eaaef92a36a890c1b7763fc76d42d
nhantt <nhant0707@gmail.com>
2016-11-21 10:14:39 +0700

    Update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  def edit
+      @user = User.find(params[:id])
+   end
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
     def user_params
