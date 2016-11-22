fc9c5ac983420d53f30ac279aae5ca6e6dcfd74c
Ly Duc Long <longlyduc@gmail.com>
2016-11-22 13:20:27 +0000

    defaul is_admin = false

--- db/schema.rb
+++ db/schema.rb
@@ -97,9 +97,9 @@ ActiveRecord::Schema.define(version: 20161120104737) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "is_admin"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.boolean  "is_admin",        default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
